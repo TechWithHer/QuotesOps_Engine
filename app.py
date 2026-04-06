dd0c0b5e0c5ddb5d3dca8690e773615af3c930e0
Ayushi <ayushi2530@gmail.com>
2026-04-06 20:44:48 +0800

    health addition

--- app.py
+++ app.py
@@ -34,3 +34,11 @@ def api():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
+
+
+@app.route("/health")
+def health():
+    return jsonify({
+        "status": "ok",
+        "service": "quotesops"
+    }), 200
